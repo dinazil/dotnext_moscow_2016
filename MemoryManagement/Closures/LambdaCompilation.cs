f6cd507e6a57a0dca9f7a75425d9d3912b5c9d80
Dina Goldshtein <dinazil@gmail.com>
2016-08-13 16:51:26 +0300

    Added code just to show how lambda compiles

---
+++ MemoryManagement/Closures/LambdaCompilation.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System;
+
+namespace Closures
+{
+    class Helper
+    {
+        public void foo() { }
+    }
+
+    class LambdaCompilation
+    {
+        private void goo()
+        {
+            var h = new Helper();
+            Action f = () => h.foo();
+            f();
+        }
+    }
+}
