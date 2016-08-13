3edfdce535a14030de8bd23d00a85eb3f6b3f7d2
Dina Goldshtein <dinazil@gmail.com>
2016-08-13 14:46:32 +0300

    Moved Measure to separate project

--- MemoryManagement/Boxing/Program.cs
+++ MemoryManagement/Boxing/Program.cs
@@ -1,40 +1,22 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Linq;
+using Utilities;
 
 namespace Boxing
 {
     class Program
     {
-        private const int Repetitions = 100;
         private const int Elements = 10000000;
-
-        private static void Measure<T>(string description, Func<T> initialization, Action<T> action)
-        {
-            var t = initialization();
-            action(t); // warmup
-
-            var sw = Stopwatch.StartNew();
-
-            for (int i = 0; i < Repetitions; ++i)
-            {
-                action(t);
-            }
-
-            sw.Stop();
-            Console.WriteLine($"{description} took {sw.ElapsedMilliseconds / Repetitions}ms");
-        }
 
         static void Main(string[] args)
         {
-            Measure("Find struct",
+            Measurements.Measure("Find struct",
                 () => Enumerable.Range(0, Elements)
                 .Select(i => new Number { Value = i })
                 .ToList(),
                 list => list.IndexOf(list.Last())
             );
 
-            Measure("Find equatable struct",
+            Measurements.Measure("Find equatable struct",
                 () => Enumerable.Range(0, Elements)
                 .Select(i => new ENumber { Value = i })
                 .ToList(),
---
+++ MemoryManagement/Utilities/Measurements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Utilities
+{
+    public static class Measurements
+    {
+        private const int Repetitions = 100;
+
+        public static void Measure<T>(string description, Func<T> initialization, Action<T> action)
+        {
+            var t = initialization();
+            action(t); // warmup
+
+            var sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < Repetitions; ++i)
+            {
+                action(t);
+            }
+
+            sw.Stop();
+            Console.WriteLine($"{description} took {sw.ElapsedMilliseconds / Repetitions}ms");
+        }
+    }
+}
